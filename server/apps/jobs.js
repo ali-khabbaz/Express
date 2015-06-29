ccc47405065e786d24c89c23380cdfa8c7b60c43
ali <ali.khabbaz14@gmail.com>
2015-06-29 15:33:55 +0430

    fix jobs server app

--- server/apps/jobs.js
+++ server/apps/jobs.js
@@ -8,7 +8,12 @@
 				message: 'you are not authorized'
 			});
 		}
-		var token = req.headers.authorization.split('ali is just.')[1];
+		var token = '';
+		if (req.headers.authorization.indexOf('ali is just.') !== -1) {
+			token = req.headers.authorization.split('ali is just.')[1];
+		} else if (req.headers.authorization.indexOf('ali is just') !== -1) {
+			token = req.headers.authorization.split('ali is just')[1];
+		}
 		var payload = jwt.decode(token, "shh...");
 		if (!payload.sub) {
 			return res.status(401).send({
